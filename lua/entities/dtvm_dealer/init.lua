91b8013ede69a29ed77ae64fbc198a1fddd75aae
nosharp <nosharp1@gmail.com>
2021-05-29 12:25:42 +0100

    feat: add vehicle dealer npc

--- lua/entities/dtvm_dealer/init.lua
+++ lua/entities/dtvm_dealer/init.lua
@@ -2,5 +2,13 @@ include("shared.lua")
 AddCSLuaFile("shared.lua")
 
 function ENT:Initialize()
-    
+    self:SetModel("models/Humans/Group03m/Female_02.mdl")
+	self:SetHullType(HULL_HUMAN)
+	self:SetHullSizeNormal()
+	self:SetNPCState(NPC_STATE_SCRIPT)
+	self:SetSolid(SOLID_BBOX)
+	self:CapabilitiesAdd(bit.bor(CAP_ANIMATEDFACE, CAP_TURN_HEAD))
+	self:SetUseType(SIMPLE_USE)
+	self:DropToFloor()
+	self:SetMaxYawSpeed(90)
 end
--- lua/entities/dtvm_dealer/shared.lua
+++ lua/entities/dtvm_dealer/shared.lua
@@ -1,3 +1,7 @@
+ENT.Type = "ai"
+ENT.Base = "base_ai"
+
+ENT.PrintName = "Vehicle Dealer NPC"
+ENT.Category = "Vehicle Dealear"
 ENT.Author = "nosharp"
-ENT.Category = "DTVM"
 ENT.Spawnable = true
